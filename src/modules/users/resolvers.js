1637fe4b39b13e2dd35d51d8533511c21da3fc10
JosephCrisostomo <joseph.crisostomo@dashlabs.ai>
2022-09-10 23:39:27 +0800

    Move two factor auth api calls to server

--- src/modules/users/resolvers.js
+++ src/modules/users/resolvers.js
@@ -1,9 +1,9 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { skip } from 'graphql-resolvers';
-import { ForbiddenError } from 'apollo-server-express';
+import { ApolloError, ForbiddenError } from 'apollo-server-express';
 import { combineResolvers } from 'graphql-resolvers';
-
+import axios from 'axios';
 
 const isAuthenticated = (parent, args, { user }) => {
     return user ? skip : new ForbiddenError('Not authenticated as user.');
@@ -25,29 +25,33 @@ const resolvers = {
         appName: () => process.env.APP_NAME,
         users: combineResolvers(
             isAdmin,
-            async (parent, args, { models }) => await models.User.find(),
+            async (_, __, { models }) => await models.User.find(),
         ),
         user: combineResolvers(
             isAuthenticated,
-            async (parent, args, { models }) => await models.User.findById(args.id)
+            async (_, {id}, { models }) => await models.User.findById(id)
         ),
     },
     Mutation: {
-        validateTwoFactorAuth: async (parent, args, { appName }) => {
-            const { code, secretCode } = args;
-            const isValid = await validateTwoFactorAuth(code, secretCode);
-            return isValid;
-        },
         createUser: 
-            async (parent, args, { models }) => {
+            async (_, { username, password }, { models }) => {
 
-                const hashedPassword = await bcrypt.hash(args.password, 10); 
-                const user = new models.User({
-                    username: args.username,
-                    password: hashedPassword,
-                    secretCode: args.secretCode,
-                    twoFactorAuthQrLink: args.twoFactorAuthQrLink
+                const hashedPassword = await bcrypt.hash(password, 10); 
+                const secretCode = await getSecretCode(process.env.GOOGLE_AUTH_API_KEY);
+                const twoFactorAuthQrLink = await getTwoFactorAuthQrLink({
+                    googleAuthApiKey: process.env.GOOGLE_AUTH_API_KEY,
+                    appName: process.env.APP_NAME,
+                    username,
+                    secretCode
                 });
+                
+                const user = new models.User({
+                    username,
+                    password: hashedPassword,
+                    secretCode,
+                    twoFactorAuthQrLink
+                });
+                
                 await user.save();
                 return user;
             },
@@ -66,30 +70,117 @@ const resolvers = {
         }),
         deleteUser: combineResolvers(
             isAuthenticated,
-            async (parent, args, { models }) => {
-            const user = await models.User.findById(args.id);
+            async (_, {id}, { models }) => {
+            const user = await models.User.findById(id);
             await user.remove();
             return user;
         }),
-        signIn: async (parent, args, { models, secret }) => {
-            const loggedUser = await models.User.findOne({ username: args.username });
+        signIn: async (_, {username, password}, { models, secret }) => {
+            const loggedUser = await models.User.findOne({ username: username });
             if (!loggedUser) {
                 throw new Error('User not found');
             }
-            const isValid = await bcrypt.compare(args.password, loggedUser.password);
+            const isValid = await bcrypt.compare(password, loggedUser.password);
             if (!isValid) {
                 throw new Error('Invalid password');
             }
 
-            return { 
+            return {
+                code: 200,
+                success: true,
+                message: 'User logged in successfully',
                 token: createToken(loggedUser, secret, '1hr'),
                 user: loggedUser
             };
+        },
+        validateCode: async(_, { userId, code }, { models, secret }) => {
+            const user = await models.User.findById(userId);
+            const isValid = await validateTwoFactorAuth(process.env.GOOGLE_AUTH_API_KEY, user.secretCode, code);
+            if(!isValid) {
+                throw new ApolloError('Invalid code');
+            }
+            return {
+                code: 200,
+                success: true,
+                message: 'Code validated successfully',
+                token: createToken(user, secret, '1hr'),
+                user: user
+            }
         }
     }
 };
 
 
 
+async function getSecretCode(googleAuthApiKey) {
+    const options = {
+        method: 'POST',
+        url: 'https://google-authenticator.p.rapidapi.com/new_v2/',
+        headers: {
+            'X-RapidAPI-Key': googleAuthApiKey,
+            'X-RapidAPI-Host': 'google-authenticator.p.rapidapi.com'
+        }
+    };
+    
+    let response = '';
+
+    try {
+        response = await axios.request(options)
+    } catch (err) {
+        throw new ApolloError('Error Generating Secret Code');
+    }
+    return response.data
+}
+
+async function getTwoFactorAuthQrLink({googleAuthApiKey, appName, username, secretCode}) {
+    const encodedParams = new URLSearchParams();
+    encodedParams.append("secret", secretCode);
+    encodedParams.append("account", username);
+    encodedParams.append("issuer", appName);
+
+    const options = {
+        method: 'POST',
+        url: 'https://google-authenticator.p.rapidapi.com/enroll/',
+        params: {secret: googleAuthApiKey, issuer: appName, account: username},
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'X-RapidAPI-Key': googleAuthApiKey,
+            'X-RapidAPI-Host': 'google-authenticator.p.rapidapi.com'
+        },
+        data: encodedParams
+    }
+
+    let response = '';
+    try {
+        response = await axios.request(options)
+    } catch (err) {
+        throw new ApolloError('Error Generating QR Code Link');
+    }
+    return response.data
+}
+
+async function validateTwoFactorAuth(googleAuthApiKey, secretCode, code){
+    const encodedParams = new URLSearchParams();
+    encodedParams.append("secret", secretCode);
+    encodedParams.append("code", code);
+
+    const options = {
+        method: 'POST',
+        url: 'https://google-authenticator.p.rapidapi.com/validate/',
+        params: {code: code, secret: secretCode},
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'X-RapidAPI-Key': googleAuthApiKey,
+            'X-RapidAPI-Host': 'google-authenticator.p.rapidapi.com'
+        },
+        data: encodedParams
+    };
+
+    axios.request(options).then(function (response) {
+        return 'TRUE' === response.data
+    }).catch(function (error) {
+        throw new ApolloError('Google Auth API Error');
+    });
+}
 
 export default resolvers;
